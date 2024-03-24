f6e0e4a38e0514324548710c438b66ff28ec1153
Richard Cândido <rich_dacan@protonmail.com>
2024-03-24 10:07:55 -0300

    feat: scope ex 12 and 13

---
+++ ex0013/index.c
@@ -0,0 +1,7 @@
+// 13. Faça um algoritmo para ler 50 números e armazenar em um vetor VET, verificar e escrever se existem números repetidos no vetor VET e em que posições se encontram.
+
+#include <stdio.h>
+
+int main() {
+
+}
